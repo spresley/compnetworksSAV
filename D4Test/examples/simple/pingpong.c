e9d6a3120cba39c2e0d4be7779267c5da4c47775
Dominic Maskell <dommaskell@hotmail.co.uk>
2015-10-13 17:41:14 +0100

    Testing D4 code Huw gave me

--- D4Test/examples/simple/pingpong.c
+++ D4Test/examples/simple/pingpong.c
@@ -2,33 +2,30 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <string.h>
-#include "avrlcd.h"
-#include "font.c"
-#include "ili934x.c"
-#include "lcd.c"
 
 #include "rfm12.h"
+#include "../uart_lib/uart.h"
 
 int main ( void )
 {
-	init_lcd();
-	set_orientation(East);
-	display_string("Loading");
-
 	uint8_t *bufptr;
 	uint8_t i;
 	uint8_t tv[] = "foobar";
 
 	uint16_t ticker = 0;
 	
-	display_string(".");
+	#ifdef LED_PORT
+		LED_DDR |= _BV(LED_BIT); //enable LED if any
+	#endif
+
+	uart_init();
+
 	_delay_ms(100);  //little delay for the rfm12 to initialize properly
 	rfm12_init();    //init the RFM12
-	display_string(".");
 	
 	sei();           //interrupts on
 
-	display_string("RFM12 Pingpong test");
+	uart_putstr ("\r\n" "RFM12 Pingpong test\r\n");
 
 	while (42) //while the universe and everything
 	{
@@ -41,17 +38,17 @@ int main ( void )
 				LED_PORT ^= _BV(LED_BIT);
 			#endif
 
-			display_string("new packet: \"");
+			uart_putstr ("new packet: \"");
 
 			bufptr = rfm12_rx_buffer(); //get the address of the current rx buffer
 
 			// dump buffer contents to uart			
 			for (i=0;i<rfm12_rx_len();i++)
 			{
-				display_string( bufptr[i] );
+				uart_putc ( bufptr[i] );
 			}
 			
-			display_string("\"\r\n");
+			uart_putstr ("\"\r\n");
 			
 			// tell the implementation that the buffer
 			// can be reused for the next data.
@@ -62,7 +59,7 @@ int main ( void )
 		ticker ++;
 		if(ticker == 3000){
 			ticker = 0;
-			display_string(".\r\n");
+			uart_putstr (".\r\n");
 			rfm12_tx (sizeof(tv), 0, tv);
 		}
 
