17efca2463c63645c13712dae2701130eee0ab04
auberj <nathan@dhcp-125-247.wireless.soton.ac.uk>
2015-10-13 12:25:04 +0100

    Fixed Config Error on pingpong example code

--- examples/simple/config.h
+++ examples/simple/config.h
@@ -46,8 +46,93 @@
 */
 
 
-#include "config.h"
+/************************
+ * RFM12 PIN DEFINITIONS
+ */
 
+//Pin that the RFM12's slave select is connected to
+#define DDR_SS DDRB
+#define PORT_SS PORTB
+#define BIT_SS 2
+
+//SPI port
+#define DDR_SPI DDRB
+#define PORT_SPI PORTB
+#define PIN_SPI PINB
+#define BIT_MOSI 3
+#define BIT_MISO 4
+#define BIT_SCK  5
+#define BIT_SPI_SS 2
+//this is the hardware SS pin of the AVR - it 
+//needs to be set to output for the spi-interface to work 
+//correctly, independently of the CS pin used for the RFM12
+
+
+/************************
+ * RFM12 CONFIGURATION OPTIONS
+ */
+
+//baseband of the module (either RFM12_BAND_433, RFM12_BAND_868 or RFM12_BAND_912)
+#define RFM12_BASEBAND RFM12_BAND_433
+
+//center frequency to use (+- FSK frequency shift)
+#define RFM12_FREQUENCY       433170000UL
+
+//Transmit FSK frequency shift in kHz
+#define FSK_SHIFT             125000
+
+//Receive RSSI Threshold
+#define RFM12_RSSI_THRESHOLD  RFM12_RXCTRL_RSSI_79
+
+//Receive Filter Bandwidth
+#define RFM12_FILTER_BW       RFM12_RXCTRL_BW_400
+
+//Output power relative to maximum (0dB is maximum)
+#define RFM12_POWER           RFM12_TXCONF_POWER_0
+
+//Receive LNA gain
+#define RFM12_LNA_GAIN        RFM12_RXCTRL_LNA_6
+
+//crystal load capacitance - the frequency can be verified by measuring the
+//clock output of RFM12 and comparing to 1MHz.
+//11.5pF seems to be o.k. for RFM12, and 10.5pF for RFM12BP, but this may vary.
+#define RFM12_XTAL_LOAD       RFM12_XTAL_11_5PF
+
+//use this for datarates >= 2700 Baud
+#define DATARATE_VALUE        RFM12_DATARATE_CALC_HIGH(9600.0)
+
+//use this for 340 Baud < datarate < 2700 Baud
+//#define DATARATE_VALUE      RFM12_DATARATE_CALC_LOW(1200.0)
+
+//TX BUFFER SIZE
+#define RFM12_TX_BUFFER_SIZE  30
+
+//RX BUFFER SIZE (there are going to be 2 Buffers of this size for double_buffering)
+#define RFM12_RX_BUFFER_SIZE  30
+
+
+/************************
+ * RFM12 INTERRUPT VECTOR
+ * set the name for the interrupt vector here
+ */
+
+//the interrupt vector
+#define RFM12_INT_VECT (INT1_vect)
+
+//the interrupt mask register
+#define RFM12_INT_MSK GICR
+
+//the interrupt bit in the mask register
+#define RFM12_INT_BIT (INT1)
+
+//the interrupt flag register
+#define RFM12_INT_FLAG GIFR
+
+//the interrupt bit in the flag register
+#define RFM12_FLAG_BIT (INTF1)
+
+//setup the interrupt to trigger on negative edge
+#define RFM12_INT_SETUP()   MCUCR |= (1<<ISC11)
 
 
 /************************
@@ -56,6 +141,8 @@
 
 #define RFM12_LIVECTRL 0
 #define RFM12_LIVECTRL_CLIENT 0
+#define RFM12_LIVECTRL_HOST 0
+#define RFM12_LIVECTRL_LOAD_SAVE_SETTINGS 0
 #define RFM12_NORETURNS 0
 #define RFM12_NOCOLLISIONDETECTION 0
 #define RFM12_TRANSMIT_ONLY 0
@@ -67,6 +154,7 @@
 #define RFM12_USE_POWER_CONTROL 0
 #define RFM12_LOW_POWER 0
 #define RFM12_USE_CLOCK_OUTPUT 0
+#define RFM12_LOW_BATT_DETECTOR 0
 
 
 #define RFM12_LBD_VOLTAGE             RFM12_LBD_VOLTAGE_3V0
@@ -92,13 +180,13 @@
 //The TX-Part can also be used to control a TX-LED with the nomral RFM12
 
 /*
-	#define RX_INIT_HOOK  DDRA |= _BV(PA1)
-	#define RX_LEAVE_HOOK PORTA &= ~_BV(PA1)
-	#define RX_ENTER_HOOK PORTA |= _BV(PA1)
+	#define RX_INIT_HOOK  DDRD |= _BV(PD5)
+	#define RX_LEAVE_HOOK PORTD &= ~_BV(PD5)
+	#define RX_ENTER_HOOK PORTD |= _BV(PD5)
 
-	#define TX_INIT_HOOK  DDRA |= _BV(PA2)
-	#define TX_LEAVE_HOOK PORTA &= ~_BV(PA2)
-	#define TX_ENTER_HOOK PORTA |= _BV(PA2)
+	#define TX_INIT_HOOK  DDRD |= _BV(PD4)
+	#define TX_LEAVE_HOOK PORTD &= ~_BV(PD4)
+	#define TX_ENTER_HOOK PORTD |= _BV(PD4)
 */
 
 /************************
