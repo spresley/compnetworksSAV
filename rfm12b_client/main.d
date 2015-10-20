7e9074d0d7b82e3c5169a7c35b9cbcc76422934c
Dominic Maskell <dommaskell@hotmail.co.uk>
2015-10-20 12:13:48 +0100

    Client and host folders

---
+++ rfm12b_client/main.d
@@ -0,0 +1,5 @@
+main.o main.d : main.cpp lib/libtft-cpp/tft.h lib/libtft-cpp/ili9341.h \
+ lib/libtft-cpp/connection.h lib/libtft-cpp/ascii.h \
+ lib/libtft-cpp/rgbconv.h rfm12.h rfm12_config.h rfm12lib/rfm12.h \
+ rfm12lib/include/rfm12_core.h rfm12lib/include/rfm12_extra.h \
+ rfm12lib/include/rfm12_livectrl.h
---
+++ rfm12b_client/rfm12.d
@@ -0,0 +1,6 @@
+rfm12.o rfm12.d : rfm12.cpp rfm12_config.h rfm12lib/rfm12.cpp \
+ rfm12lib/include/rfm12_hw.h rfm12lib/include/rfm12_core.h \
+ rfm12lib/rfm12.h rfm12lib/include/rfm12_extra.h \
+ rfm12lib/include/rfm12_livectrl.h rfm12lib/include/rfm12_spi.c \
+ rfm12lib/include/rfm12_spi_linux.c rfm12lib/include/rfm12_livectrl.c \
+ rfm12lib/include/rfm12_extra.c
